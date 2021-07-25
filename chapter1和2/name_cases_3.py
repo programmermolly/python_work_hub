33ae108d3555509c038eaf0c03174012c9d37f76
programmerjx <jxkdy123@126.com>
2021-07-25 21:21:27 +0800

    经典教材代码

---
+++ chapter1和2/name_cases_3.py
@@ -0,0 +1,2 @@
+# To be or not be，That is a question—— W. William Shakespeare
+print('W. William Shakespeare once said,"To be or not be，That is a question"')
---
+++ chapter1和2/simple_message.py
@@ -0,0 +1,2 @@
+message='人生苦短，我用Python'
+print(message)
